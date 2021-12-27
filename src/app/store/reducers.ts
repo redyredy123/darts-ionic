acbbcdfcc0e6d88eb180ab52f6de83408e09cdbc
Michał Gawryś <mg@bcave.io>
2021-12-27 19:20:24 +0100

    :heavy_plus_sign: localstorage sync for rehydrating state

--- src/app/store/reducers.ts
+++ src/app/store/reducers.ts
@@ -5,6 +5,7 @@ import {
 	createSelector,
 	MetaReducer,
 } from '@ngrx/store';
+import { Keys, localStorageSync } from 'ngrx-store-localstorage';
 import { environment } from '../../environments/environment';
 import { PlayerState, players } from './players';
 
@@ -16,6 +17,15 @@ export const reducers: ActionReducerMap<State> = {
 	players,
 };
 
-export const metaReducers: MetaReducer<State>[] = !environment.production
-	? []
-	: [];
+const STORE_KEYS_TO_PERSIST: Keys = ['players'];
+
+export const localStorageSyncReducer = (
+	reducer: ActionReducer<State>
+): ActionReducer<State> =>
+	localStorageSync({
+		keys: STORE_KEYS_TO_PERSIST,
+		// keys: [],
+		rehydrate: true,
+	})(reducer);
+
+export const metaReducers: MetaReducer<State>[] = [localStorageSyncReducer];
